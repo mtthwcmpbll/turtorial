3564ef0a603f3a78d1e3207771884bcdbf5aaf73
Matthew Campbell <matt@moderne.io>
2026-01-02 10:52:42 -0500

    Initial commit

---
+++ src/main/java/com/snowfort/turtorial/TurtorialApplication.java
@@ -0,0 +1,13 @@
+package com.snowfort.turtorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurtorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snowfort/turtorial/TurtorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowfort.turtorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
