1656af89f9f119a6f45e72aac5408380e84d707b
Matthew Campbell <matt@123elm.com>
2026-02-01 22:54:25 -0500

    fixed tests

--- src/main/frontend/vite.config.ts
+++ src/main/frontend/vite.config.ts
@@ -29,15 +29,19 @@ export default defineConfig({
       }
     }
   },
+  optimizeDeps: {
+    include: ['@testing-library/react', '@testing-library/dom']
+  },
   test: {
     projects: [{
       extends: true,
       plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
+        // The plugin will run tests for the stories defined in your Storybook config
+        // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+        storybookTest({
+          configDir: path.join(dirname, '.storybook')
+        })
+      ],
       test: {
         name: 'storybook',
         browser: {
